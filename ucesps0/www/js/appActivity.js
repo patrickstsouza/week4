5a0dd6b40d8d0e3c7efd4ec5f441cb7612027b61
Patrick Santos de Souza <patrickstsouza@gmail.com>
2018-02-20 21:19:24 +0000

    added earthquake data

--- ucesps0/www/js/appActivity.js
+++ ucesps0/www/js/appActivity.js
@@ -1,3 +1,47 @@
+// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable
+var client;
+// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
+var earthquakelayer;
+
+var testMarkerRed = L.AwesomeMarkers.icon({
+    icon: 'play',
+    markerColor: 'red'
+});
+var testMarkerPink = L.AwesomeMarkers.icon({
+    icon: 'play',
+    markerColor: 'pink'
+});
+
+// create the code to get the Earthquakes data using an XMLHttpRequest
+function getEarthquakes() {
+    client = new XMLHttpRequest();
+    client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
+    client.onreadystatechange = earthquakeResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
+    client.send();
+}
+
+// create the code to wait for the response from the data server, and process the response once it is received
+function earthquakeResponse() {
+    // this function listens out for the server to say that the data is ready - i.e. has state 4
+    if (client.readyState == 4) {
+        // once the data is ready, process the data
+        var earthquakedata = client.responseText;
+        loadEarthquakelayer(earthquakedata);
+    }
+}
+
+// convert the received data - which is text - to JSON format and add it to the map
+function loadEarthquakelayer(earthquakedata) {
+    // convert the text received from the server to JSON
+    var earthquakejson = JSON.parse(earthquakedata );
+    // load the geoJSON layer
+    var earthquakelayer = L.geoJson(earthquakejson).addTo(mymap);
+    bounds = earthquakelayer.getBounds();
+    if (bounds.isValid()) {
+        mymap.fitBounds(earthquakelayer.getBounds());
+    }
+}
+
 // load the map
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
@@ -10,26 +54,3 @@ L.tileLayer(
             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.streets'
     }).addTo(mymap);
-
-
-// add a point
-L.marker([51.5, -0.09]).addTo(mymap)
-    .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-// add a circle
-L.circle([51.508, -0.11], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a circle.");
-
-// add a polygon with 3 end points (i.e. a triangle)
-var myPolygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a polygon.");
